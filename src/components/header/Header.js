a30b55bc35dc9dc9ea434e0c8ddcfe68db5cd57f
dinhthanhhai <dthk164@gmail.com>
2023-04-10 20:27:45 +0700

    Add

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import ThemeSwitcher from "./ThemeSwitcher";
 const Header = ({ HandleThemeChange }) => {
   return (
     <header>
-      <h1>calc</h1>
+      <h1>Calculator</h1>
       <ThemeSwitcher HandleThemeChange={HandleThemeChange} />
     </header>
   );
